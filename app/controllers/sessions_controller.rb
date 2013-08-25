fff3b535b401ec55f3102de0317632bdf2acbdf9
Yuuj M <yuujmaruyama@me.com>
2013-08-24 22:34:57 -0700

    sign-in/out @ 8.2.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			sign_in user       # this shit doesnt exist :(
+			redirect_to user   # dont redirect to user object. Use user/(:id)
+		else
+			flash.now[:error] = 'Invalid email/password combo'
+			render 'new'
+		end
 	end
 
 	def destroy
