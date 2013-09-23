697c94fcb2385725c6ac1ac882c79ce0c7965867
Yuuj M <yuujmaruyama@me.com>
2013-09-22 17:30:14 -0700

    @10.45 feed tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,11 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  def feed
+    # Prelim code setup following later "Following Users"
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     def create_remember_token
